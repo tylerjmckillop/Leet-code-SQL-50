136d5419cd4d25b844856d49fd15abd9e3787693
tylerjmckillop <tyler.mckillop18@gmail.com>
2026-01-06 14:30:40 +0000

    Time: 259 ms (18.33%) | Memory: 0B (100.00%) - LeetSync

--- 1801-average-time-of-process-per-machine/average-time-of-process-per-machine.sql
+++ 1801-average-time-of-process-per-machine/average-time-of-process-per-machine.sql
@@ -2,25 +2,10 @@
 
 #average time each machine takes to complete a process
 
-WITH sumtimetable AS
-( 
-SELECT
-    machine_id
-    ,process_id
-    ,SUM(CASE WHEN activity_type = 'start' THEN `timestamp` END) AS sum_start
-    ,SUM(CASE WHEN activity_type = 'end'   THEN `timestamp` END) AS sum_end
-FROM Activity
-GROUP BY machine_id, process_id
-ORDER BY machine_id, process_id)
-, per_process AS(
-    SELECT   machine_id
-            ,process_id
-            ,sum_end - sum_start as processing_time1
-    FROM sumtimetable
-)
-
 SELECT 
-        machine_id
-        ,ROUND(AVG(processing_time1), 3) as processing_time 
-FROM per_process 
-GROUP BY machine_id
+    a1.machine_id,
+    ROUND(AVG(a2.timestamp - a1.timestamp), 3) as processing_time
+FROM activity a1
+JOIN activity a2 ON a1.machine_id = a2.machine_id AND a1.process_id = a2.process_id
+WHERE a1.activity_type = 'start' AND a2.activity_type = 'end'
+GROUP BY a1.machine_id, a2.machine_id
