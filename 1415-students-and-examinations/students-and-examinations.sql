e58c02fda5dbdc203b66b70cccf67180a7887304
tylerjmckillop <tyler.mckillop18@gmail.com>
2026-01-06 15:10:08 +0000

    Time: 1007 ms (26.29%) | Memory: 0B (100.00%) - LeetSync

--- 1415-students-and-examinations/students-and-examinations.sql
+++ 1415-students-and-examinations/students-and-examinations.sql
@@ -1,16 +1,28 @@
+
+
+
+/* SELECT 
+e.student_id,
+e.subject_name,
+COUNT(e.subject_name) AS number_attended
+FROM Examinations e 
+LEFT JOIN students s1 ON s1.student_id = e.student_id
+GROUP BY e.student_id, e.subject_name */
+
+
+/* SELECT *
+FROM examinations e
+RIGHT JOIN students s1 ON s1.student_id = e.student_id */
+
+
+
 SELECT 
-
-s.student_id
-,s.student_name
-,sub.subject_name
-,COUNT(e.subject_name) as attended_exams
-
-FROM students s
-CROSS JOIN subjects sub
-LEFT JOIN examinations e 
-ON e.student_id = s.student_id AND e.subject_name = sub.subject_name
-
-GROUP BY s.student_id, sub.subject_name
-ORDER BY s.student_id, sub.subject_name
-
-
+    s1.student_id,
+    s1.student_name,
+    s2.subject_name,
+    COUNT(e.student_id) AS attended_exams
+FROM students s1 
+CROSS JOIN subjects s2 
+LEFT JOIN examinations e ON s1.student_id = e.student_id AND s2.subject_name = e.subject_name
+GROUP BY s1.student_id, s2.subject_name
+ORDER BY student_id, subject_name
