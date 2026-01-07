df399da1093fa4934eb57e92568965ab6676c98a
tylerjmckillop <tyler.mckillop18@gmail.com>
2026-01-07 10:38:19 +0000

    Time: 334 ms (48.72%) | Memory: 0B (100.00%) - LeetSync

--- 570-managers-with-at-least-5-direct-reports/managers-with-at-least-5-direct-reports.sql
+++ 570-managers-with-at-least-5-direct-reports/managers-with-at-least-5-direct-reports.sql
@@ -1,25 +1,8 @@
 # Write your MySQL query statement below
 
-#Each Row is Name of Employee, Department, and ID of manager.
-
-#IF Manager ID is null then employee does not have a manager 
-
-
-#Solution to find managers with at least 5 direct reports
-
-WITH Reports AS
-    (
-SELECT 
-    manageriD
-    ,COUNT(*) as direct_reports
-FROM Employee 
-WHERE managerId IS NOT NULL
-GROUP BY managerId
-HAVING COUNT(*) >= 5
-    )
-
-SELECT 
-    name
-FROM Employee e 
-INNER JOIN Reports r 
-    ON r.managerID = e.iD
+SELECT
+    e1.name
+FROM Employee e1
+INNER JOIN Employee e2 ON e1.id = e2.managerId
+GROUP BY e1.name, e1.id
+HAVING COUNT(e2.id) >= 5
