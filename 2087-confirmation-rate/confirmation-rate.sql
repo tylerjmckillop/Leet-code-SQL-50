a3a58772e1e230b2d715ed0ca04c737a81691cae
tylerjmckillop <tyler.mckillop18@gmail.com>
2026-01-07 11:30:23 +0000

    Time: 658 ms (28.55%) | Memory: 0B (100.00%) - LeetSync

--- 2087-confirmation-rate/confirmation-rate.sql
+++ 2087-confirmation-rate/confirmation-rate.sql
@@ -3,7 +3,7 @@
 
 SELECT
     s.user_id,
-    ROUND((SUM(IF(c.action = 'confirmed', 1, 0))) / COUNT(s.user_id), 2) as confirmation_rate
+    ROUND((AVG(IF(c.action = 'confirmed', 1, 0))), 2) as confirmation_rate
 FROM Signups s 
 LEFT JOIN Confirmations c ON s.user_id = c.user_id
 GROUP BY s.user_id 
