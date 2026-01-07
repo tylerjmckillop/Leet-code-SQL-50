61f22e900cc99c9e7e9a3bd6e2330d7dfcc403b3
tylerjmckillop <tyler.mckillop18@gmail.com>
2026-01-07 16:43:59 +0000

    Time: 640 ms (27.89%) | Memory: 0B (100.00%) - LeetSync

--- 1292-immediate-food-delivery-ii/immediate-food-delivery-ii.sql
+++ 1292-immediate-food-delivery-ii/immediate-food-delivery-ii.sql
@@ -1,15 +1,39 @@
 # Write your MySQL query statement below
-SELECT
-  ROUND(SUM(is_immediate) * 100.0 / COUNT(*), 2) AS immediate_percentage
-FROM (
-  SELECT
-    (d.order_date = d.customer_pref_delivery_date) AS is_immediate
-  FROM Delivery d
-  JOIN (
-    SELECT customer_id, MIN(order_date) AS first_order_date
-    FROM Delivery
-    GROUP BY customer_id
-  ) m
-    ON d.customer_id = m.customer_id
-   AND d.order_date  = m.first_order_date
-) x;
+
+# pref date = order date --> immediate
+# pref date != order date --> scheduled
+
+#first order is order with earliest order date
+
+#percentage of immediate orders in the FIRST order of all customers
+
+-- SELECT 
+--     *,
+--     IF(order_date = customer_pref_delivery_date, 'immediate', 'scheduled') as IorS,
+--     MIN(order_date)
+-- FROM Delivery
+-- GROUP BY customer_Id
+
+-- SELECT
+--     d1.customer_id,
+--     MIN(d1.order_date),
+--     MIN(d1.customer_pref_delivery_date),
+    
+--     IF(min(d1.order_date) = min(d1.customer_pref_delivery_date), 1, 2)as IorS
+-- FROM Delivery d1
+-- JOIN 
+--     (Delivery d2
+-- ON d1.order_date = d2.order_date AND d1.customer_id = d2.customer_id
+-- GROUP BY d1.customer_Id
+
+SELECT 
+    ROUND(AVG(d.order_date = d.customer_pref_delivery_date) * 100 ,2) as immediate_percentage
+FROM Delivery d
+JOIN
+    (
+SELECT customer_id, MIN(order_date) AS first_order_date
+  FROM Delivery
+  GROUP BY customer_id
+    ) f
+ON d.customer_id = f.customer_id
+ AND d.order_date = f.first_order_date
