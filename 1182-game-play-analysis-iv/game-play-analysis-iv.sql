36c17d6a7b8fed41f082c423a1cc652f227c40d2
tylerjmckillop <tyler.mckillop18@gmail.com>
2026-01-08 10:51:23 +0000

    Time: 621 ms (20.38%) | Memory: 0B (100.00%) - LeetSync

--- 1182-game-play-analysis-iv/game-play-analysis-iv.sql
+++ 1182-game-play-analysis-iv/game-play-analysis-iv.sql
@@ -1,20 +1,24 @@
 # Write your MySQL query statement below
 
-#Each players first day
-WITH first_login AS (
-  SELECT
-    player_id,
-    MIN(event_date) AS first_day
-  FROM Activity
-  GROUP BY player_id
-)
-SELECT
-  ROUND(
-    1.0 * COUNT(DISTINCT f.player_id)
-    / (SELECT COUNT(*) FROM first_login),
-    2
-  ) AS fraction
-FROM first_login f
-JOIN Activity a2
-  ON a2.player_id = f.player_id
- AND a2.event_date = DATE_ADD(f.first_day, INTERVAL 1 DAY);
+
+#To get number of players
+-- SELECT COUNT(DISTINCT player_id)
+-- FROM Activity 
+
+SELECT 
+        ROUND((
+        SELECT COUNT(DISTINCT b.player_id) as immediate_login
+        FROM Activity a
+        JOIN 
+            (SELECT
+                player_id,
+                MIN(event_date) as first_day,
+                DATE_ADD(MIN(event_date), INTERVAL 1 DAY) as dayafter
+            FROM Activity
+            GROUP BY player_id
+            ) b
+        ON 
+            a.player_id = b.player_id AND
+            a.event_date = b.dayafter
+            )/COUNT(DISTINCT player_id),2) as fraction
+FROM Activity 
