f7593a3ec342e67974798d932e21390897c59226
Tyler <tyler.mckillop18@gmail.com>
2026-01-20 11:58:13 +0000

    Time: 840 ms (8.21%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1811-fix-names-in-a-table/fix-names-in-a-table.sql
@@ -0,0 +1,7 @@
+# Write your MySQL query statement below
+
+    SELECT 
+        user_id,
+        CONCAT(UCASE(SUBSTRING(name, 1, 1)), LOWER(SUBSTRING(name, 2))) as name
+    FROM Users
+    ORDER BY user_id asc
